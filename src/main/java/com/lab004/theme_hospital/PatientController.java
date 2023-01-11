e546d50314d5a577dc6b5e5521911185299683b6
fef <fef@mail.com>
2023-01-11 16:42:55 +0100

    PatientController terminé, à vérifier

--- src/main/java/com/lab004/theme_hospital/PatientController.java
+++ src/main/java/com/lab004/theme_hospital/PatientController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.lab004.theme_hospital.Service.PatientService;
+import com.lab004.theme_hospital.models.Consultation;
 import com.lab004.theme_hospital.models.Medecin;
+import com.lab004.theme_hospital.models.Patient;
 
 @RestController
 @RequestMapping(path="/api/patients")
@@ -27,5 +29,51 @@ public class PatientController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	}
-		
+	
+	@GetMapping(path="/")
+	public ResponseEntity<Medecin> getMedecinByNom(String nom) {
+		try {
+			return new ResponseEntity<>(patientService.getMedecinByNom(nom), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PostMapping(path="/")
+	public ResponseEntity<Patient> savePatient(@RequestBody Patient patient) {
+		try {
+			return new ResponseEntity<>(patientService.savePatient(patient), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@PutMapping(path = "/{id}")
+    public ResponseEntity<Patient>  updatePatient(@PathVariable Long id,@RequestBody Patient patient) {
+        try {
+            return new ResponseEntity<Patient>(patientService.updatePatient(id, patient), HttpStatus.OK) ;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+	
+	@PutMapping(path = "/{id}")
+    public ResponseEntity<Consultation>  updateConsultation(@PathVariable Long id,@RequestBody Consultation consultation) {
+        try {
+            return new ResponseEntity<Consultation>(patientService.updateConsultation(id, consultation), HttpStatus.OK) ;
+        } catch (Exception e) {
+            // TODO: handle exception
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+	
+	
+	@DeleteMapping(path="/{id}")
+	public void deletePatient(@PathVariable Long id) {
+		patientService.deletePatient(id);
+	}
+	
 }	
--- src/main/java/com/lab004/theme_hospital/Service/PatientService.java
+++ src/main/java/com/lab004/theme_hospital/Service/PatientService.java
@@ -21,5 +21,4 @@ public interface PatientService {
 	
 	void deletePatient(Long id);
 
-	
 }
